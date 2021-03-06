81bf487b44d20d665ad5046ae485cc52cd6b7ad7
nicolas f <fournout.nicolas@gmail.com>
2021-03-06 14:28:09 +0100

    .

--- TP5/tableau.s
+++ TP5/tableau.s
@@ -12,9 +12,18 @@ main:
    mov r1, #22
    add r0, r0, #4
    str r1, [r0]
-   
 
-   @ a completer
+   mov r1, #33
+   add r0, r0, #4
+   str r1, [r0]
+   
+   mov r1, #44
+   add r0, r0, #4
+   str r1, [r0]
+   
+   mov r1, #55
+   add r0, r0, #4
+   str r1, [r0]
 
 fin:  BX LR
 
